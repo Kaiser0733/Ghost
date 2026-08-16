84345c3b7a488d543b4ad8b412f085c68a0e4bc4
Kaiser0733 <amardeepkumar0733@gmail.com>
2026-08-16 21:08:40 +0530

    fix(ci): remove repositories block from settings.gradle.kts
    - repositories {} is not valid in settings.gradle.kts (only in build.gradle.kts)
    - pluginManagement.repositories is the correct place for plugin resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,11 +12,6 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23" apply false
 }
 
-repositories {
-    google()
-    mavenCentral()
-}
-
 rootProject.name = "Ghost"
 
 include("ble-feasibility-lab")
